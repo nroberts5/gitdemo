e15af5618a26269c42e2ef486cee8cf04be75276
Nate Roberts <nroberts5@wisc.edu>
2020-08-26 16:15:26 -0500

    Improved main

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Hello World!")
-print("This is a new feature!\nAdjustment #1\nAdjustment #2")
+
+
+if __name__ == '__main__':
+	print("Hello World!")
+	print("This is a new feature!\nAdjustment #1\nAdjustment #2")
